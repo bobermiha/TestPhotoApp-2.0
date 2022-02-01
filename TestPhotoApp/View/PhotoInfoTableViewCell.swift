07b2ecda95bf963573d0d614ab60a046f68884f4
bobermiha <bobermiha@yandex.ru>
2022-02-01 20:00:58 +0500

    Initial Commit

---
+++ TestPhotoApp/View/PhotoInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoInfoTableViewCell.swift
+//  TestPhotoApp
+//
+//  Created by Михаил Бобров on 01.02.2022.
+//
+
+import UIKit
+
+class PhotoInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
